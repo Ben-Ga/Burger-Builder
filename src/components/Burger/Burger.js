8d2b6cacfdec7496966898ff19a850e174d86c16
Ben Gallagher <b8009285@newcastle.ac.uk>
2020-04-18 12:02:48 +0100

    Added ingredients state, ingredients can be dynamically rendered

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+import BurgerIngredient from "../Burger/BurgerIngredient/BurgerIngredient";
+
+import classes from "../../styles/components/burger.module.scss";
+
+const Burger = (props) => {
+  const transformedIngredients = Object.keys(props.ingredients).map((ingrd) => {
+    return [...Array(props.ingredients[ingrd])].map((_, i) => {
+      return <BurgerIngredient key={ingrd + i} ingredientType={ingrd} />;
+    });
+  }).reduce((arr, elm) =>{ return arr.concat(elm)}, [])
+   //turn the object passed, into set of arrays of length the value that each key was assigned in state of BurgerBuilder
+   //the reduce function then flattens the array, allowing us to check its length properly
+
+  return (
+    <div className={classes.burger}>
+      <BurgerIngredient ingredientType="bread-top" />
+      {transformedIngredients.length > 0 ? transformedIngredients : <p>Please start adding ingredients</p>}
+      <BurgerIngredient ingredientType="bread-bottom" />
+    </div>
+  );
+};
+
+export default Burger;
--- src/containers/burgerbuilder/BurgerBuilder.js
+++ src/containers/burgerbuilder/BurgerBuilder.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react'
 import Auxiliary from '../../hoc/Auxiliary'
+import Burger from '../../components/Burger/Burger'
 
 export default class BurgerBuilder extends Component {
+
+
+    constructor(props){
+        super(props);
+        this.state = {
+            ingredients: {
+                // salad: 1,
+                // bacon: 1,
+                // meat: 2,
+                // cheese: 1,
+            }
+        }
+
+    }
     render() {
         return (
             <Auxiliary>
                 <div>
-                    Burger
+                    <Burger 
+                        ingredients={this.state.ingredients}
+                    />
                 </div>
                 <div>
                     Build Controls
